6d6b5b51fec2f935cbf0c4eb41ad830edf47ba4e
Anderson <andersonxu88@gmail.com>
2019-12-03 16:06:20 +1030

    build

---
+++ precache-manifest.c61a5bf0a151ab44a9f0e54219da8b01.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348b2e7cc22dac6e3e3d7d533ec032a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "999b76610e1c49198715",
+    "url": "/static/js/2.1f6d1930.chunk.js"
+  },
+  {
+    "revision": "bf10731741f6dd7d4829",
+    "url": "/static/js/main.1f05b5f3.chunk.js"
+  },
+  {
+    "revision": "42c67b91adc7ac55185c",
+    "url": "/static/js/runtime-main.58660c70.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "andersonxu.github.io/catalyst.github.io/precache-manifest.114521eefad741c39148f44511df1b1f.js"
+  "/precache-manifest.c61a5bf0a151ab44a9f0e54219da8b01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("andersonxu.github.io/catalyst.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
